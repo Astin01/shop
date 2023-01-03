4d551a2930f2749745742310d2d6cc6fa1a0f5a7
astin <wonhun1225@gmail.com>
2023-01-03 22:13:31 +0900

    event banner by using useEffect

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import { useState } from "react";
 import data from "./data.js";
 import { Shoe } from "./main-shoe.js";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import { Product } from "./product.js";
 
 function App() {
+  const navigate = useNavigate();
   let [shoes] = useState(data);
   let shoesdata = shoes.map((data) => (
     <Shoe id={data.id} title={data.title} price={data.price}></Shoe>
   ));
-
   let product = shoes.map((data) => (
     <Product
       id={data.id}
@@ -26,9 +26,9 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/detail">Product</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
+            <Nav.Link onClick={() => navigate("/detail")}>Product</Nav.Link>
+            <Nav.Link onClick={() => navigate("/event")}>Event</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -44,10 +44,23 @@ function App() {
             </div>
           }
         />
-        <Route path="/detail" element={<>{product}</>} />
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+        <Route path="/detail/:parms" element={<Product shoes={shoes} />} />
+        {/* <Route path="*" element={<div>없는페이지임</div>} /> */}
       </Routes>
     </div>
   );
 }
 
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
 export default App;
--- src/product.js
+++ src/product.js
@@ -1,19 +1,44 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Product(props) {
+  let [alert, setAlert] = useState(true);
+  let [input, setInput] = useState(0);
+  useEffect(() => {
+    setTimeout(() => setAlert(false), 2000);
+  }, []);
+  let { parms } = useParams();
+  function isId(element) {
+    if (element.id == parms) {
+      return true;
+    }
+  }
+  let onChange = useEffect((e) => {}, [input]);
+  const product = props.shoes.find(isId);
   return (
     <div className="container">
+      {alert == true ? (
+        <div className="alert alert-warning">2초 이내 구매시 할인</div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img
-            src={`https://codingapple1.github.io/shop/shoes${props.id + 1}.jpg`}
+            src={`https://codingapple1.github.io/shop/shoes${
+              product.id + 1
+            }.jpg`}
             width="100%"
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.title}</h4>
-          <p>{props.content}</p>
-          <p>{props.price}</p>
+          <h4 className="pt-5">{product.title}</h4>
+          <p>{product.content}</p>
+          <p>{product.price}</p>
+          {isNaN(input) ? (
+            alert("숫자만 입력해주세요")
+          ) : (
+            <input type="text" onChange={onChange} />
+          )}
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
